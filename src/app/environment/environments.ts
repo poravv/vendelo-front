1e3aec3d0a4040ffd551c7ff317779d2493c51c4
elporavv <andyvercha@gmail.com>
2024-09-04 16:57:02 -0400

    Update parse formatt

--- src/app/environment/environments.ts
+++ src/app/environment/environments.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     apiUrl: 'https://kc.mindtechpy.net/admin/realms/realm_vendelo',
-    //serverUrl: 'http://localhost:3000/vendelo/api',
-    serverUrl: 'https://back.mindtechpy.net/vendelo/vendelo/api',
+    serverUrl: 'http://localhost:3000/vendelo/api',
+    //serverUrl: 'https://back.mindtechpy.net/vendelo/vendelo/api',
     keycloakConfig: {
         clientId:"client-vendelo",
         issuer: 'https://kc.mindtechpy.net/realms/realm_vendelo',
